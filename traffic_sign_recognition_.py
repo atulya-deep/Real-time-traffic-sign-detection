6a2ba4e30be272bf16645ffa06f2cdc921f3ccb7
ATULYA DEEP <83969166+atulya-deep@users.noreply.github.com>
2022-11-23 23:11:50 +0530

    Update traffic_sign_recognition_.py

--- traffic_sign_recognition_.py
+++ traffic_sign_recognition_.py
@@ -220,3 +220,25 @@ pd.DataFrame(history.history).plot(figsize=(8, 5))
 plt.grid(True)
 plt.gca().set_ylim(0, 1)
 plt.show()
+
+test = pd.read_csv(data_dir + '/Test.csv')
+
+labels = test["ClassId"].values
+imgs = test["Path"].values
+
+data =[]
+for img in imgs:
+    try:
+        image = cv2.imread(data_dir + '/' +img)
+        image_fromarray = Image.fromarray(image, 'RGB')
+        resize_image = image_fromarray.resize((IMG_HEIGHT, IMG_WIDTH))
+        data.append(np.array(resize_image))
+    except:
+        print("Error in " + img)
+X_test = np.array(data)
+X_test = X_test/255
+
+pred = model.predict_classes(X_test)
+
+#Accuracy with the test data
+print('Test Data accuracy: ',accuracy_score(labels, pred)*100)
